21267caea0e5433091ab46379f398b8d9d14443d
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-09-18 03:35:43 +0200

    Initial implementation

--- src/example.ts
+++ src/example.ts
@@ -4,20 +4,11 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-import { ProjectRoot } from './project-root.ts';
+import { projectRoot } from './project-root.ts';
 
-export const example = () => {
-  // Print methods
-  const l = console.log;
-  const h1 = (text: string) => l(`${text}`);
-  const h2 = (text: string) => l(`  ${text.split('\n')}`);
-  const p = (text: string) => l(`    ${text}`);
-
-  // Example
-  h1('ProjectRoot.example');
-  h2('Returns an instance of the ProjectRoot.');
-  const example = ProjectRoot.example;
-  p(JSON.stringify(example, null, 2));
+export const example = async () => {
+  const pr = await projectRoot();
+  console.log(pr);
 };
 
 /*
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-export { ProjectRoot } from './project-root.ts';
+export * from './project-root.ts';
